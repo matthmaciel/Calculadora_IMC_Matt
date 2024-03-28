0a47b4b64c55eeeed036b704fa99909f0e79d9df
matheus <matheus_coelhomaciel@hotmail.com>
2024-03-28 18:01:33 -0300

    Create Calculadora_IMC_MaTT.py

---
+++ Calculadora_IMC_MaTT.py
@@ -0,0 +1,7 @@
+Peso = float (input('Digite seu peso em kg: '))
+Altura = float (input('Digite sua altura em metros: '))
+Idade = int (input('Digite sua idade: '))
+
+imc = Peso / (Altura ** 2)
+
+print (imc)
